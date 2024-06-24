160be89778558edd16435b4c571872b1933980c2
Ebube-Ochemba <ebube17@gmail.com>
2024-06-24 05:50:33 +0100

    for Filestorage testing

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,15 @@
 """
 Initializes the models package and loads the storage engine
 """
-from models.storage_factory import get_storage
+from models.engine.file_storage import FileStorage
 
 
-storage = get_storage()
+storage = FileStorage()
 storage.reload()
+
+
+
+
+# from models.storage_factory import get_storage
+# storage = get_storage()
+# storage.reload()
