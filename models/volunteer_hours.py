a632f22e24befa6e4db5907ca2048263d5cb0532
Ebube-Ochemba <ebube17@gmail.com>
2024-06-26 00:19:30 +0100

    done, pre sql

---
+++ models/volunteer_hours.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines the VolunteerHours class"""
+
+from models.base_model import BaseModel
+
+
+class VolunteerHours(BaseModel):
+    """Represents Volunteer Hours"""
+    volunteer_id = ""
+    event_id = ""
+    hours = 0.0
+
+    def __init__(self, *args, **kwargs):
+        """Initializes VolunteerHours"""
+        super().__init__(*args, **kwargs)
+        if "hours" in kwargs and isinstance(kwargs["hours"], (int, float)):
+            if kwargs["hours"] < 0:
+                raise ValueError("Hours must be a positive number")
+            self.hours = kwargs["hours"]
+
+    def __setattr__(self, name, value):
+        """Sets attributes, ensuring hours are positive"""
+        if name == "hours":
+            if isinstance(value, (int, float)):
+                if value < 0:
+                    raise ValueError("Hours must be a positive number")
+        super().__setattr__(name, value)
