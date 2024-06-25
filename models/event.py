42aee0722e26d62d445eadc6d2d9c9427abfb594
Ebube-Ochemba <ebube17@gmail.com>
2024-06-25 23:12:21 +0100

    complete, pre sql

---
+++ models/event.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module creates the Event class"""
+
+from models.base_model import BaseModel
+from datetime import datetime, timezone
+
+
+class Event(BaseModel):
+    """Represents an Event"""
+    title = ""
+    description = ""
+    date = datetime.now(timezone.utc)
+    location = ""
+    creator_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes event"""
+        super().__init__(*args, **kwargs)
+        if 'date' in kwargs:
+            self.date = kwargs['date']
+
+    def __setattr__(self, name, value):
+        """Sets a date with validation"""
+        if name == "date":
+            if isinstance(value, str):
+                # Try to parse the date string
+                try:
+                    value = datetime.fromisoformat(value)
+                except ValueError:
+                    raise ValueError("Invalid date format. Use ISO 8601")
+            elif not isinstance(value, datetime):
+                raise TypeError("date must be a datetime object")
+            if value.tzinfo is None:
+                value = value.replace(tzinfo=timezone.utc)
+        super().__setattr__(name, value)
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+        new_dict = super().to_dict()
+        if "date" in new_dict and isinstance(new_dict["date"], datetime):
+            new_dict["date"] = new_dict["date"].isoformat()
+        return new_dict
