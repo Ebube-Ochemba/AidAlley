a94cc5cb3b93c5643d91fa3514134c0b4f47d54b
Ebube-Ochemba <ebube17@gmail.com>
2024-06-27 00:15:12 +0100

    updated for DBStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Initializes the models package and loads the storage engine
+Factory: Chooses the storage engine based on an environment variable.
+          Initializes the models package and loads the storage engine
 """
-from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
+storage_type = getenv("STORAGE_TYPE")
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
-
-
-
-
-# from models.storage_factory import get_storage
-# storage = get_storage()
-# storage.reload()
