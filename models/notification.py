70d505c84370c3921c83e25454c3c01de5ad7422
Ebube-Ochemba <ebube17@gmail.com>
2024-06-26 02:31:44 +0100

    done, pre sql

---
+++ models/notification.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module defines the Notification class"""
+
+from models.base_model import BaseModel
+
+class Notification(BaseModel):
+    """Represents a Notification"""
+    volunteer_id = ""
+    message = ""
+    status = "unread"  # Default status
+
+    def __init__(self, *args, **kwargs):
+        """Initializes Notification"""
+        super().__init__(*args, **kwargs)
+
+    def mark_as_read(self):
+        """Marks the notification as read"""
+        self.status = "read"
+        self.save()
+
+    def mark_as_dismissed(self):
+        """Marks the notification as dismissed"""
+        self.status = "dismissed"
+        self.save()
