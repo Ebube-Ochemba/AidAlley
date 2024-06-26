7f198bd5e9396675aa1990f6e7acd48b87c8478f
Ebube-Ochemba <ebube17@gmail.com>
2024-06-26 03:22:22 +0100

    date format error

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Test BaseModel for expected behavior and documentation"""
 from datetime import datetime
+from models.volunteer import BaseModel
 import inspect
 import models
-import pycodestyle
 import time
-import unittest
 from unittest import mock
+import inspect
+import pycodestyle
+import unittest
 
-BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
 
 
