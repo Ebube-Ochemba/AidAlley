e73a71e482420bba7e8cae08f3958aaf3577bd95
Ebube-Ochemba <ebube17@gmail.com>
2024-07-04 03:26:01 +0100

    update after get and count tweak

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,11 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.event import Event
+from models.event_volunteer import EventVolunteer
+from models.notification import Notification
+from models.volunteer import Volunteer
+from models.volunteer_hours import VolunteerHours
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,10 +20,10 @@ def get_status():
 def get_stats():
     """Returns the number of each object by type"""
     stats = {
-        "volunteer": storage.count('Volunteer'),
-        "event": storage.count('Event'),
-        "event_volunteer": storage.count('EventVolunteer'),
-        "notification": storage.count('Notification'),
-        "volunteer_hours": storage.count('VolunteerHours')
+        "event": storage.count(Event),
+        "event_volunteer": storage.count(EventVolunteer),
+        "notification": storage.count(Notification),
+        "volunteer": storage.count(Volunteer),
+        "volunteer_hours": storage.count(VolunteerHours)
     }
     return jsonify(stats)
