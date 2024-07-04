5c1b2d144edb2a8a57808bb9bd1080f52bdc42af
Ebube-Ochemba <ebube17@gmail.com>
2024-07-04 03:45:40 +0100

    all views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,8 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__)
 
 from api.v1.views.index import *
+from api.v1.views.events import *
+from api.v1.views.event_volunteers import *
+from api.v1.views.notifications import *
+from api.v1.views.volunteers import *
+from api.v1.views.volunteer_hours import *
