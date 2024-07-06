1c71e8656a7c4f32c019c4b8a2bb9cd42efc8835
Ebube-Ochemba <ebube17@gmail.com>
2024-07-06 22:25:38 +0100

    homepage setup

---
+++ web_app/views/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creates a flask Blueprint and imports all views which use Blueprint"""
+from flask import Blueprint
+
+web_views = Blueprint('web_views', __name__)
+
+# Importing all the view modules
+from web_app.views.index import *
+from web_app.views.events import *
+# from web_app.views.volunteers import *
+# from web_app.views.event_volunteers import *
+# from web_app.views.notifications import *
+# from web_app.views.volunteer_hours import *
