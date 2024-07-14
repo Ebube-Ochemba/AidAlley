f5f88ceba30f5e2b850d07f0398f751a50f4e101
Ebube-Ochemba <ebube17@gmail.com>
2024-07-14 14:13:34 +0100

    adder separate time feature

--- web_app/views/events.py
+++ web_app/views/events.py
@@ -2,6 +2,8 @@
 
 from web_app.views import web_views
 from flask import render_template, jsonify, request, abort
+from datetime import datetime, timezone
+from dateutil import parser
 from models import storage
 from models.event import Event
 from uuid import uuid4
@@ -40,6 +42,22 @@ def event_detail(event_id):
     event = storage.get(Event, event_id)
     if event is None:
         abort(404)
+    try:
+        if isinstance(event.date, datetime):
+            event_datetime = event.date
+        else:
+            event_datetime = parser.parse(str(event.date))
+        
+        event_datetime = event_datetime.replace(tzinfo=timezone.utc)
+        event_date = event_datetime.date()
+        event_time = event_datetime.time()
+        # Format time to show only hours and minutes
+        event_time = event_datetime.strftime("%H:%M")
+    except Exception as e:
+        print(f"Error parsing date: {e}")
+
     return render_template('event-details.html',
                            cache_id=uuid4(),
-                           event=event)
+                           event=event,
+                           event_date=event_date,
+                           event_time=event_time)
