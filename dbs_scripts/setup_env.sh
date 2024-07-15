73ab376d79798d95056314f6dcdbdd1c5d2f65dc
Ebube-Ochemba <ebube17@gmail.com>
2024-07-15 17:09:49 +0100

    update

--- dbs_scripts/setup_env.sh
+++ dbs_scripts/setup_env.sh
@@ -9,6 +9,7 @@ export AIDALLEY_MYSQL_HOST='localhost'
 export AIDALLEY_MYSQL_DB='aidalley_db'
 export AIDALLEY_API_HOST=0.0.0.0
 export AIDALLEY_API_PORT=5000
+export JWT_SECRET_KEY='aidalley_jwt_secret_key'
 
 # Print the environment variables
 printenv | grep AIDALLEY
