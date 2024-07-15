61ac2628c278fd97588977c2fff5370904d7fb20
Ebube-Ochemba <ebube17@gmail.com>
2024-07-15 12:40:49 +0100

    intitial form setup

--- web_app/static/scripts/scripts.js
+++ web_app/static/scripts/scripts.js
@@ -27,3 +27,20 @@ document.querySelector('.load-more-button').addEventListener('click', function (
     })
     .catch(error => console.error('Error loading more events:', error));
 });
+
+// Toggle password visibility
+function togglePassword(inputId) {
+  const input = document.getElementById(inputId);
+  const button = input.nextElementSibling.nextElementSibling;
+  const icon = button.querySelector('i');
+
+  if (input.type === 'password') {
+      input.type = 'text';
+      icon.classList.remove('fa-eye');
+      icon.classList.add('fa-eye-slash');
+  } else {
+      input.type = 'password';
+      icon.classList.remove('fa-eye-slash');
+      icon.classList.add('fa-eye');
+  }
+}
